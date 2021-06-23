5bdd8c62230c8357ed1c87de4fbe492ea4cd1928
Dhakov <ja.pueblat@duocuc.cl>
2021-06-22 22:54:27 -0400

    Creadas y pobladas las tablas de la base de datos con datos del Software que hay en la página

--- django_imp/core/admin.py
+++ django_imp/core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Software, Categoria
 
 # Register your models here.
+
+admin.site.register(Categoria)
+admin.site.register(Software)
--- django_imp/core/models.py
+++ django_imp/core/models.py
@@ -7,10 +7,14 @@ class Categoria(models.Model):
     nomCategoria = models.CharField(max_length=50, verbose_name='Nombre de la Categoria')
     descCategoria = models.CharField(max_length=200, verbose_name='Descripcion de la Categoria')
 
-    def __str__(self) -> str:
+    def __str__(self):
         return (self.nomCategoria)
 
 class Software(models.Model):
     idSoftware = models.IntegerField(primary_key=True, verbose_name='Id del Software')
     nomSoftware = models.CharField(max_length=50, verbose_name='Nombre del Software')
     descSoftware = models.CharField(max_length=300, verbose_name='Descripcion del Software')
+    catSoftware = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return (self.nomSoftware)
