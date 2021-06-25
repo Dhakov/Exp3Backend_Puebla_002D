d7d6491eae206d63ae19321df2ec91722d523d68
Dhakov <ja.pueblat@duocuc.cl>
2021-06-25 03:46:36 -0400

    Todo terminado

--- django_imp/core/urls.py
+++ django_imp/core/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import form_software, index, software
+from . import views
+from .views import form_software, index, software, formulario, videojuego
 
 urlpatterns = [
-    path('', index, name="index"),
-    path('', software, name="software"),
-    path('form_software', form_software, name = "form_software"),
+    path('', views.index, name="index"),
+
+    path('juegos/', views.videojuego, name="videojuego"),
+    path('software/', views.software, name="software"),
+    path('core/', views.form_software, name="formulario"),
+    path('modificar/', views.verTodo, name="verTodo"),
+    path('modificar/<id>', views.mod_software, name="mod_software"),
+    path('modificar/eliminar/<id>', views.del_software, name="del_software"),
 ]
--- django_imp/core/views.py
+++ django_imp/core/views.py
@@ -6,16 +6,17 @@ from .forms import SoftwareForm
 # Create your views here.
 
 def index(request):
-
-    aplicaciones = Software.objects.all()
-
-    return render(request, 'index.html', 
-        context={'datos':aplicaciones}
-    )
+    return render(request, 'index.html')
 
 def software(request):
-    return render(request, 'software.html')
+    return render(request, 'software/software.html')
 
+def videojuego(request):
+    return render(request, 'juegos/videojuegos.html')
+
+def formulario(request):
+    software_form = SoftwareForm()
+    return render(request, 'core/ingresar_software.html', {'software_form':software_form})
 
 def form_software(request):
     if request.method == 'POST':
@@ -25,5 +26,30 @@ def form_software(request):
             return redirect('index')
     else:
         software_form = SoftwareForm()
+    
     return render(request, 'core/ingresar_software.html', {'software_form':software_form})
 
+def verTodo(request):
+    todo_software = Software.objects.all()
+
+    return render(request, 'modificar/verTodo.html',
+        context={'datos':todo_software}
+    )
+
+def mod_software(request, id):
+    software = Software.objects.get(idSoftware = id)
+
+    datos = {
+        'form' : SoftwareForm(instance=software)
+    }
+    if request.method == 'POST':
+        formulario = SoftwareForm(data=request.POST, instance=software)
+        if formulario.is_valid:
+            formulario.save()
+            return redirect('verTodo')
+    return render(request, 'modificar/modificar.html', datos)
+
+def del_software(request, id):
+    software = Software.objects.get(idSoftware = id)
+    software.delete()
+    return redirect('verTodo')
